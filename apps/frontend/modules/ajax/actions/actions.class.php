2a90374b825d8036f3f3fd91c1a0acc0fec4c2a5
Jakub Dziwoki <jakub@dziwoki.me>
2013-05-19 11:42:35 +0200

    changes

--- apps/frontend/modules/ajax/actions/actions.class.php
+++ apps/frontend/modules/ajax/actions/actions.class.php
@@ -49,29 +49,25 @@ class ajaxActions extends sfActions
   }
 
    public function executeAddpage(sfWebRequest $request) {
-  	$page_id = $request->getParameter('event_id', false);
-  	
+  	$page_id = $request->getParameter('page_id', false);
   	$facebook = Facebook::get();
    	
-   	$dataFacebook = $request->getParameter('page_id');
+   	
    	$token = $facebook->getLongToken();
-    $user_pages = $facebook->getPages($dataFacebook);
+    $user_pages = $facebook->getPages($page_id);
+
     if($user_pages) {
-	
-  	 	$dataFacebook['access_token'] = $user_pages['access_token'];
    
     	$page = new Page();
     	$user_id = $facebook->getUser()->getId();
     	$page->setUserId($user_id);
     	$page->setAccessToken($user_pages['access_token']);
-    	$page->setFacebookId($dataFacebook);
+    	$page->setFacebookId($page_id);
     	$page->save();
 
     	//$this->processForm($request, $this->form);	
     }
-    	
-
-
+    
   	echo json_encode(array("success"=>1));
   	die();
   }
